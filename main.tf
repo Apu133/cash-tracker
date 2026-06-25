ec18736fe67b00ac0b11b853483dd28aaa09f05c
apu <surenarpit133@gmail.com>
2026-06-26 01:24:00 +0530

    Added node_extractor installation automation in install.sh and added port for node_extractor in main.tf

--- main.tf
+++ main.tf
@@ -51,6 +51,12 @@ resource "aws_security_group" "ec2_sg" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        from_port = 9100
+        to_port = 9100
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     egress {
         from_port = 0
         to_port = 0
